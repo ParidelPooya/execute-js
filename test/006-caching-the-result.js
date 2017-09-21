0e91c89640177e8a6bac076f9fc2588d2f7eaa17
DESKTOP-5OOPHQ1\pooya <pooya.p@outlook.com>
2017-09-21 01:32:14 -0700

    Initial change for running steps in parallel with throttle

---
+++ test/006-caching-the-result.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let execute = require("../src/index");
+
+const func = (data)=> {
+    return new Promise((resolve, reject) => {
+        if (Math.random() > 0.8) {
+            resolve(data);
+        } else {
+            reject(null);
+        }
+    });
+};
+
+
+let executionTree = {
+    concurrency: 1,
+    steps :[
+        {
+            title:'step 1',
+            retry: 10,
+            action: (data) => func({a: 1})
+        }
+    ]
+};
+
+
+
+let executionData = {
+    sub_id :123
+};
+
+execute(executionTree, executionData).then( (result)=> {
+    console.log("finished with this result:");
+    console.log(result);
+}).catch( ()=> {
+    console.log("catch");
+});
