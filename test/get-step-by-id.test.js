36e06d1fec22a3351c9a931da518c3578f90e62b
Pooya Paridel <pooya.p@outlook.com>
2019-01-09 17:50:38 -0800

    Add an option to find step by id in child execution tree

--- test/get-step-by-id.test.js
+++ test/get-step-by-id.test.js
@@ -74,6 +74,227 @@ lab.experiment("Get Step By Id Test", () => {
         code.expect(step).to.equal(false);
     });
 
+    lab.test("should return false if step is in child execution tree and traverseChild is false", () => {
+
+        let executionTree = Execute.prepareExecutionTree([
+            {
+                title:"step 1",
+                action: (data) => {return {a: 1};}
+            },
+            {
+                id: "id2",
+                title:"step 2",
+                action: (data) => {return {b: 1};}
+            },
+            {
+                title:"step 3",
+                action: (data) => {return {c: 1};}
+            },
+            {
+                title: "step 1",
+                actionType: "map",
+                action:{
+                    array: (data) => data.array,
+                    executionTree: [
+                        {
+                            id: "id3",
+                            title: "step c1",
+                            action: (data) => ({a:data.i})
+                        },
+                        {
+                            title: "step c2",
+                            action: (data) => ({b:data.i + 1})
+                        }
+                    ]
+                }
+            }
+        ]);
+
+        let step = Execute.getStepById(executionTree, "id3", false);
+
+        code.expect(step).to.equal(false);
+    });
+
+    lab.test("should return the step if step is in child execution tree (map middleware) and traverseChild is true", () => {
+
+        let executionTree = Execute.prepareExecutionTree([
+            {
+                title:"step 1",
+                action: (data) => {return {a: 1};}
+            },
+            {
+                id: "id2",
+                title:"step 2",
+                action: (data) => {return {b: 1};}
+            },
+            {
+                title:"step 3",
+                action: (data) => {return {c: 1};}
+            },
+            {
+                title: "step 4",
+                actionType: "map",
+                action:{
+                    array: (data) => data.array,
+                    executionTree: [
+                        {
+                            title: "step c1",
+                            action: (data) => ({a:data.i})
+                        },
+                        {
+                            title: "step c2",
+                            action: (data) => ({b:data.i + 1})
+                        }
+                    ]
+                }
+            },
+            {
+                title: "step 5",
+                actionType: "map",
+                action:{
+                    array: () => [1,2,3],
+                    reducer: (data) => {return data + 1;}
+                },
+                output: {
+                    addToResult: false,
+                    accessibleToNextSteps: false,
+                }
+            },
+            {
+                title: "step 6",
+                actionType: "map",
+                action:{
+                    array: (data) => data.array,
+                    executionTree: [
+                        {
+                            title: "step c1",
+                            action: (data) => ({a:data.i})
+                        },
+                        {
+                            id: "id3",
+                            title: "step c2",
+                            action: (data) => ({b:data.i + 1})
+                        }
+                    ]
+                }
+            }
+        ]);
+
+        let step = Execute.getStepById(executionTree, "id3", true);
+
+        code.expect(step !== false).to.equal(true);
+    });
+
+    lab.test("should return the step if step is in child execution tree (child tree middleware) and traverseChild is true", () => {
+
+        let executionTree = Execute.prepareExecutionTree([
+            {
+                title:"step 1",
+                action: (data) => {return {a: 1};}
+            },
+            {
+                title:"step 2",
+                action: (data) => {return {b: 1};}
+            },
+            {
+                title:"step 3",
+                actionType: "execution-tree",
+                action: {
+                    executionTree: [
+                        {
+                            title:"step 1",
+                            action: (data) => {return {a: 1};}
+                        },
+                        {
+                            id: "id2",
+                            title:"step 2",
+                            action: (data) => {return {b: 1};}
+                        },
+                    ],
+                    executionData: (data)=> ({input : data.code})
+                }
+            }
+
+        ]);
+
+        let step = Execute.getStepById(executionTree, "id2", true);
+
+        code.expect(step !== false).to.equal(true);
+    });
+
+    lab.test("should return the step if step is in child execution tree (while middleware) and traverseChild is true", () => {
+
+        let executionTree = Execute.prepareExecutionTree([
+            {
+                title:"step 1",
+                action: (data) => {return {a: 1};}
+            },
+            {
+                title:"step 2",
+                action: (data) => {return {b: 1};}
+            },
+            {
+                title: "step c1",
+                action: (data) => (new Date()),
+                output: {
+                    map: {
+                        destination: "start"
+                    }
+                }
+            },
+            {
+                title: "step c1",
+                actionType: "while",
+                action:{
+                    test: (data) => (new Date()) - data.start < 1,
+                    executionTree: [{
+                        title: "step c1",
+                        action: () => ({a:1})
+                    }]
+                },
+                output: {
+                    map: {
+                        destination: "while_output"
+                    }
+                }
+            },
+            {
+                title: "step c2",
+                actionType: "while",
+                action:{
+                    test: (data) => (new Date()) - data.start < 2,
+                },
+                output: {
+                    map: {
+                        destination: "while_output"
+                    }
+                }
+            },
+            {
+                title: "step c2",
+                actionType: "while",
+                action:{
+                    test: (data) => (new Date()) - data.start < 5,
+                    executionTree: [{
+                        id: "id2",
+                        title: "step c1",
+                        action: () => ({a:1})
+                    }]
+                },
+                output: {
+                    map: {
+                        destination: "while_output"
+                    }
+                }
+            }
+
+        ]);
+
+        let step = Execute.getStepById(executionTree, "id2", true);
+
+        code.expect(step !== false).to.equal(true);
+    });
+
     lab.test("returns child step by passing id", () => {
 
         let executionTree = Execute.prepareExecutionTree([
